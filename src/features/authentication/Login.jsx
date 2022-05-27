72e7345fc5fcdf2a67d964267bfee7c210b0f76f
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-28 00:58:34 +0530

    add login and signup page

--- src/features/authentication/Login.jsx
+++ src/features/authentication/Login.jsx
@@ -1,3 +1,74 @@
+import {
+  Container,
+  Typography,
+  Box,
+  createTheme,
+  GrayTextP,
+} from "utils/material-ui";
+
+import { TextFormField, CustomButton, RouterLink } from "components";
+
+const theme = createTheme();
+
+theme.typography.h1 = {
+  fontSize: "1.8rem",
+  "@media (min-width:600px)": {
+    fontSize: "2rem",
+  },
+};
+
 export const Login = () => {
-  return <>Login</>;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: "white",
+          width: "100%",
+          p: 2,
+        }}
+      >
+        <Typography
+          variant="h1"
+          color="primary"
+          align="center"
+          sx={{
+            fontWeight: "bold",
+          }}
+          theme={theme}
+          gutterBottom
+        >
+          Login to AutoCrew
+        </Typography>
+        <Box component="form">
+          <TextFormField label="Username" />
+
+          <TextFormField label="Password" type="password" />
+
+          <CustomButton type="submit" fullWidth="true" variant="contained">
+            Login
+          </CustomButton>
+
+          <CustomButton fullWidth="true" variant="outlined">
+            Login as guest
+          </CustomButton>
+        </Box>
+
+        <Box my={1} mx={0.5}>
+          <GrayTextP>
+            Don't have an account?{" "}
+            <RouterLink linkText="Sign up" to="/signup" />
+          </GrayTextP>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
--- src/features/authentication/Signup.jsx
+++ src/features/authentication/Signup.jsx
@@ -1,3 +1,73 @@
+import {
+  Container,
+  Typography,
+  Box,
+  createTheme,
+  GrayTextP,
+} from "utils/material-ui";
+
+import { TextFormField, CustomButton, RouterLink } from "components";
+
+const theme = createTheme();
+
+theme.typography.h1 = {
+  fontSize: "1.8rem",
+  "@media (min-width:600px)": {
+    fontSize: "2rem",
+  },
+};
+
 export const Signup = () => {
-  return <>Signup</>;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: "white",
+          width: "100%",
+          p: 2,
+        }}
+      >
+        <Typography
+          variant="h1"
+          color="primary"
+          align="center"
+          sx={{
+            fontWeight: "bold",
+          }}
+          theme={theme}
+          gutterBottom
+        >
+          Sign up to AutoCrew
+        </Typography>
+
+        <Box component="form">
+          <TextFormField label="First name" />
+          <TextFormField label="Last name" />
+          <TextFormField label="Username" />
+          <TextFormField type="password" label="Password" />
+          <TextFormField type="password" label="Confirm password" />
+
+          <CustomButton fullWidth="true" variant="contained" type="submit">
+            Sign up
+          </CustomButton>
+        </Box>
+
+        <Box my={1} mx={0.5}>
+          <GrayTextP>
+            Have an account already?{" "}
+            <RouterLink linkText="Log in" to="/login" />
+          </GrayTextP>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
