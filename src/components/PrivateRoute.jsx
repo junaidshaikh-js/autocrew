00ff52ff3f5c307c0149c3c88b6640bf6465b5c6
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-28 20:11:51 +0530

    update private route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,14 +1,36 @@
 import { Navigate, useLocation, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+import { Container } from "utils/material-ui";
+import { SideNavigation } from "./SideNavigation";
+import { SideInfoContainer } from "./SideInfoContainer";
+import { Grid } from "utils/material-ui";
+
 export const PrivateRoute = () => {
   const location = useLocation();
   const { token } = useSelector((store) => store.authDetail);
 
   return token ? (
-    <div>
-      <Outlet />
-    </div>
+    <Container
+      maxWidth="lg"
+      sx={{
+        mt: "1rem",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item md={3} sm={4}>
+          <SideNavigation />
+        </Grid>
+
+        <Grid item md={6} sm={8} xs={12}>
+          <Outlet />
+        </Grid>
+
+        <Grid item md={3}>
+          <SideInfoContainer />
+        </Grid>
+      </Grid>
+    </Container>
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
   );
