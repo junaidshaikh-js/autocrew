f2a46fe0112fdb804e5dd59cd4be0ed8c1203524
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-29 18:20:11 +0530

    update user detail call to firebase

--- src/firebase/firebase-calls.js
+++ src/firebase/firebase-calls.js
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { writeBatch, doc, getDoc } from "firebase/firestore";
+import { writeBatch, doc, getDoc, updateDoc } from "firebase/firestore";
+
+import { updateUserDetailState } from "features/user-data/userSlice";
 import { db } from "./config";
 
 export const createUserDb = async (
@@ -13,8 +15,20 @@ export const createUserDb = async (
     const batch = writeBatch(db);
 
     const userDataRef = doc(db, userId, "userData");
+    const usersRef = doc(db, "users", userId);
 
     batch.set(userDataRef, {
+      firstName: firstName,
+      lastName: lastName,
+      fullName: firstName + " " + lastName,
+      userName,
+      email,
+      bio: "",
+      profilePicture: "",
+      website: "",
+    });
+
+    batch.set(usersRef, {
       firstName: firstName,
       lastName: lastName,
       fullName: firstName + " " + lastName,
@@ -47,3 +61,17 @@ export const getUserDetail = createAsyncThunk(
     }
   }
 );
+
+export const updateUserDetail = async (userId, updatedUserDetail, dispatch) => {
+  try {
+    const userDataRef = doc(db, userId, "userData");
+    const usersRef = doc(db, "users", userId);
+
+    await updateDoc(userDataRef, updatedUserDetail);
+    await updateDoc(usersRef, updatedUserDetail);
+
+    dispatch(updateUserDetailState(updatedUserDetail));
+  } catch (error) {
+    console.log(error);
+  }
+};
