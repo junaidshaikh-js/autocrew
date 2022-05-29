e45d016fe10c4a49687040c57ba5a1eb22f9094e
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-30 00:15:33 +0530

    add post component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,37 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { AddPost } from "./AddPost";
+import { getUserDetail } from "../firebase/firebase-calls";
+import { Box, CircularProgress } from "utils/material-ui";
+
 export const Home = () => {
-  return <>Home</>;
+  const { token } = useSelector((store) => store.authDetail);
+  const { userDetailLoading } = useSelector((store) => store.userDetail);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (userDetailLoading === "idle") {
+      dispatch(getUserDetail(token));
+    }
+  }, [dispatch, userDetailLoading, token]);
+
+  return (
+    <Box component="main">
+      {userDetailLoading === "loading" ? (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "60vh",
+          }}
+        >
+          <CircularProgress size="4rem" />
+        </Box>
+      ) : (
+        <AddPost />
+      )}
+    </Box>
+  );
 };
