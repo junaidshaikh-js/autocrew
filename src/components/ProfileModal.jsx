765098f0dd22b2edaaced8f629039ccdb95fa16e
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-29 18:22:36 +0530

    add profile update modal

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateUserDetail } from "../firebase/firebase-calls";
 
+import { updateUserDetail } from "../firebase/firebase-calls";
 import {
   Box,
   Typography,
