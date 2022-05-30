c79a4fe43b8f130fccccd117c07a28d118efb7cf
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-31 02:21:17 +0530

    firebase calls for new post, get all posts and get all users

--- src/firebase/firebase-calls.js
+++ src/firebase/firebase-calls.js
@@ -1,5 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { writeBatch, doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  writeBatch,
+  doc,
+  getDoc,
+  updateDoc,
+  addDoc,
+  collection,
+  arrayUnion,
+  getDocs,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
 import { updateUserDetailState } from "features/user-data/userSlice";
 import { db } from "./config";
@@ -15,9 +26,10 @@ export const createUserDb = async (
     const batch = writeBatch(db);
 
     const userDataRef = doc(db, userId, "userData");
+    const userPostsRef = doc(db, userId, "posts");
     const usersRef = doc(db, "users", userId);
 
-    batch.set(userDataRef, {
+    const userData = {
       firstName: firstName,
       lastName: lastName,
       fullName: firstName + " " + lastName,
@@ -26,18 +38,11 @@ export const createUserDb = async (
       bio: "",
       profilePicture: "",
       website: "",
-    });
+    };
 
-    batch.set(usersRef, {
-      firstName: firstName,
-      lastName: lastName,
-      fullName: firstName + " " + lastName,
-      userName,
-      email,
-      bio: "",
-      profilePicture: "",
-      website: "",
-    });
+    batch.set(userDataRef, userData);
+    batch.set(usersRef, userData);
+    batch.set(userPostsRef, { posts: [] });
 
     await batch.commit();
   } catch (error) {
@@ -87,3 +92,51 @@ export const updateUserDetail = async (userId, updatedUserDetail, dispatch) => {
     console.log(error);
   }
 };
+
+export const addNewPost = async (userId, postText, postImage, dispatch) => {
+  try {
+    const userPostsRef = doc(db, userId, "posts");
+
+    const docRef = await addDoc(collection(db, "posts"), {
+      postBy: userId,
+      postText: postText,
+      postImageUrl: postImage.url,
+      postImageName: postImage.fileName || "",
+      dateCreated: Date.now(),
+      likes: 0,
+      comments: [],
+    });
+
+    await updateDoc(userPostsRef, { posts: arrayUnion(docRef.id) });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getAllPosts = createAsyncThunk("posts/getAllPosts/", async () => {
+  const posts = [];
+
+  const postsRef = collection(db, "posts");
+
+  const q = query(postsRef, orderBy("dateCreated", "desc"));
+
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    posts.push(doc.data());
+  });
+
+  return posts;
+});
+
+export const getAllUsers = createAsyncThunk("users/getAllUsers", async () => {
+  const users = [];
+
+  const querySnapshot = await getDocs(collection(db, "users"));
+
+  querySnapshot.forEach((doc) => {
+    users.push({ id: doc.id, data: doc.data() });
+  });
+
+  return users;
+});
