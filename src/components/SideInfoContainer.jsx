74ed3fe9e382401e58ffc1cd8a88e3d3f3ac5319
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-31 23:37:47 +0530

    side bar component shows all the users the current user cna follow

--- src/components/SideInfoContainer.jsx
+++ src/components/SideInfoContainer.jsx
@@ -1,6 +1,19 @@
+import { useSelector } from "react-redux";
+
 import { Box, Typography, theme } from "utils/material-ui";
+import { UserCard } from "./UserCard";
 
 export const SideInfoContainer = () => {
+  const { users } = useSelector((store) => store.users);
+  const {
+    userDetails: { following },
+  } = useSelector((store) => store.userDetail);
+  const { token } = useSelector((store) => store.authDetail);
+
+  const usersToRecommend = users.filter(
+    (user) => !following.following.includes(user.id) && user.id !== token
+  );
+
   return (
     <Box
       component="aside"
@@ -10,6 +23,7 @@ export const SideInfoContainer = () => {
         display: { xs: "none", md: "block" },
         position: "sticky",
         top: "1rem",
+        py: 1,
       }}
     >
       <Typography
@@ -23,6 +37,20 @@ export const SideInfoContainer = () => {
       >
         Who to follow?{" "}
       </Typography>
+
+      <Box component="section">
+        {!usersToRecommend.length ? (
+          <Box textAlign="center" py="1rem">
+            <Typography variant="h4" fontSize="1.2rem">
+              No users to follow
+            </Typography>
+          </Box>
+        ) : (
+          usersToRecommend.map((user) => {
+            return <UserCard user={user} key={user.id} />;
+          })
+        )}
+      </Box>
     </Box>
   );
 };
