373c124df86f1ffc62e7590316854e5473bf44aa
Junaid <sj.shaikhjunaid@gmail.com>
2022-05-31 23:34:05 +0530

    explore page - user can see all post by all users

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,3 +1,34 @@
+import { useSelector } from "react-redux";
+
+import { Post } from "features";
+import { Box, Typography, theme } from "utils/material-ui";
+
 export const Explore = () => {
-  return <>Explore</>;
+  const { posts } = useSelector((store) => store.posts);
+
+  return (
+    <Box
+      component="main"
+      sx={{
+        border: 1,
+      }}
+    >
+      <Box component="header" my={2} mx={1}>
+        <Typography
+          variant="h1"
+          theme={theme}
+          sx={{
+            fontWeight: "bold",
+          }}
+        >
+          Explore
+        </Typography>
+      </Box>
+      <Box component="section">
+        {posts.map((post) => (
+          <Post postData={post} key={post.dateCreated} />
+        ))}
+      </Box>
+    </Box>
+  );
 };
