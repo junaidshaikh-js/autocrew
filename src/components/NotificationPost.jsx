2d19eb12d009cfddfc6ab7681a2ea420cf9f5f30
Junaid <sj.shaikhjunaid@gmail.com>
2022-06-05 01:44:32 +0530

    [fix] notification for deleted posts

--- src/components/NotificationPost.jsx
+++ src/components/NotificationPost.jsx
@@ -7,86 +7,100 @@ export const NotificationPost = ({ postByUser, post }) => {
   const { postText, postImageUrl, postImageName } = post?.data || {};
 
   return (
-    <Box
-      component={Link}
-      to={`/post/${post?.id}`}
-      sx={{
-        border: 1,
-        mt: 1,
-        textDecoration: "none",
-        color: "currentcolor",
-      }}
-    >
-      <Stack
-        component="section"
-        direction="row"
-        spacing={2}
-        sx={{
-          px: 1,
-          py: 1,
-        }}
-      >
-        <Box>
-          <Avatar
-            sx={{
-              bgcolor: "#1565C0",
-              width: 30,
-              height: 30,
-              objectFit: "contain",
-              border: 1,
-            }}
-            src={postByUser?.data.profilePicture}
-          />
-        </Box>
-
+    <>
+      {!post ? (
         <Box
           sx={{
-            flexGrow: 1,
+            border: 1,
+            mt: 1,
+            p: 1,
+          }}
+        >
+          <Typography>Post Deleted</Typography>
+        </Box>
+      ) : (
+        <Box
+          component={Link}
+          to={`/post/${post?.id}`}
+          sx={{
+            border: 1,
+            mt: 1,
+            textDecoration: "none",
+            color: "currentcolor",
           }}
         >
           <Stack
-            spacing={1}
+            component="section"
+            direction="row"
+            spacing={2}
             sx={{
-              mt: 1,
-              mb: "0.5rem",
-              flexDirection: { sm: "column", md: "row" },
+              px: 1,
+              py: 1,
             }}
           >
-            <Typography>{postByUser?.data.fullName}</Typography>
-            <GrayTextP>
-              @{postByUser?.data.userName} •{" "}
-              {getPostTime(post?.data.dateCreated)}
-            </GrayTextP>
-          </Stack>
-
-          <Box>
-            <Typography>{postText}</Typography>
-
-            {postImageUrl && (
-              <Box
-                component="figure"
+            <Box>
+              <Avatar
                 sx={{
-                  width: { sm: "0%", md: "80%" },
-                  m: 0,
-                  mt: 4,
-                  border: 1,
-                  borderRadius: "10px",
+                  bgcolor: "#1565C0",
+                  width: 30,
+                  height: 30,
                   objectFit: "contain",
-                  overflow: "hidden",
+                  border: 1,
+                }}
+                src={postByUser?.data.profilePicture}
+              />
+            </Box>
+
+            <Box
+              sx={{
+                flexGrow: 1,
+              }}
+            >
+              <Stack
+                spacing={1}
+                sx={{
+                  mt: 1,
+                  mb: "0.5rem",
+                  flexDirection: { sm: "column", md: "row" },
                 }}
               >
-                <img
-                  src={postImageUrl}
-                  alt={postImageName}
-                  loading="lazy"
-                  height="auto"
-                  width="100%"
-                />
+                <Typography>{postByUser?.data.fullName}</Typography>
+                <GrayTextP>
+                  @{postByUser?.data.userName} •{" "}
+                  {getPostTime(post?.data.dateCreated)}
+                </GrayTextP>
+              </Stack>
+
+              <Box>
+                <Typography>{postText}</Typography>
+
+                {postImageUrl && (
+                  <Box
+                    component="figure"
+                    sx={{
+                      width: { sm: "0%", md: "80%" },
+                      m: 0,
+                      mt: 4,
+                      border: 1,
+                      borderRadius: "10px",
+                      objectFit: "contain",
+                      overflow: "hidden",
+                    }}
+                  >
+                    <img
+                      src={postImageUrl}
+                      alt={postImageName}
+                      loading="lazy"
+                      height="auto"
+                      width="100%"
+                    />
+                  </Box>
+                )}
               </Box>
-            )}
-          </Box>
+            </Box>
+          </Stack>
         </Box>
-      </Stack>
-    </Box>
+      )}
+    </>
   );
 };
