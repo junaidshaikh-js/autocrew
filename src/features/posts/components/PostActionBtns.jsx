818a2262b9b70dd9d2554fa8bc45885be5fa870e
Junaid <sj.shaikhjunaid@gmail.com>
2022-06-04 21:26:10 +0530

    [refractor] post component

---
+++ src/features/posts/components/PostActionBtns.jsx
@@ -0,0 +1,105 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { handlePostLike, handleBookmark } from "../utils";
+import {
+  Stack,
+  IconButton,
+  FavoriteBorderIcon,
+  CommentIcon,
+  BookmarkBorderIcon,
+  FavoriteIcon,
+  BookmarkIcon,
+  Typography,
+} from "utils/material-ui";
+
+export const PostActionBtns = ({
+  postId,
+  postBy,
+  post,
+  setShowCommentSection,
+}) => {
+  const {
+    userDetails: { likedPost, bookmarks },
+  } = useSelector((store) => store.userDetail);
+  const { token } = useSelector((store) => store.authDetail);
+  const dispatch = useDispatch();
+
+  const isPostLikedByCurrentUser = likedPost.likedPost.includes(postId);
+  const isPostBookmarkedByCurrentUser = bookmarks.bookmarks.includes(postId);
+  const { likes, comments } = post?.data || {};
+
+  return (
+    <Stack
+      direction="row"
+      spacing={2}
+      sx={{
+        justifyContent: "space-between",
+        mx: 1,
+        mt: 2,
+      }}
+    >
+      <IconButton
+        aria-label="Like"
+        onClick={() => {
+          isPostLikedByCurrentUser
+            ? handlePostLike("dec", postId, dispatch, token, postBy)
+            : handlePostLike("inc", postId, dispatch, token, postBy);
+        }}
+      >
+        {isPostLikedByCurrentUser ? (
+          <FavoriteIcon
+            sx={{
+              color: "red",
+            }}
+          />
+        ) : (
+          <FavoriteBorderIcon />
+        )}
+        {likes > 0 && (
+          <Typography
+            variant="span"
+            sx={{
+              ml: 1,
+              fontSize: "0.875rem",
+            }}
+          >
+            {likes}
+          </Typography>
+        )}
+      </IconButton>
+
+      <IconButton
+        aria-label="Comment"
+        onClick={() => setShowCommentSection((s) => !s)}
+      >
+        <CommentIcon />
+        {comments.length !== 0 && (
+          <Typography
+            variant="span"
+            sx={{
+              ml: 1,
+              fontSize: "0.875rem",
+            }}
+          >
+            {comments.length}
+          </Typography>
+        )}
+      </IconButton>
+
+      <IconButton
+        aria-label="Bookmark"
+        onClick={() => {
+          isPostBookmarkedByCurrentUser
+            ? handleBookmark("remove", postId, token, dispatch)
+            : handleBookmark("add", postId, token, dispatch);
+        }}
+      >
+        {isPostBookmarkedByCurrentUser ? (
+          <BookmarkIcon />
+        ) : (
+          <BookmarkBorderIcon />
+        )}
+      </IconButton>
+    </Stack>
+  );
+};
--- src/features/posts/utils.js
+++ src/features/posts/utils.js
@@ -1,3 +1,21 @@
+import toast from "react-hot-toast";
+
+import {
+  bookmarkPost,
+  dislikePost,
+  likePost,
+  removePostFromBookmark,
+} from "../../firebase/firebase-calls";
+
+import {
+  updateLikedPosts,
+  updateLikedPostsForDislike,
+  addToBookmark,
+  removeFromBookmark,
+} from "features/user-data/userSlice";
+
+import { updatePostLike } from "features/posts/postSlice";
+
 export const getPostTime = (dateCreated) => {
   let now = Date.now();
   let numberOfMillisecondsPassed = now - dateCreated;
@@ -27,4 +45,42 @@ export const getPostTime = (dateCreated) => {
   }
 
   if (days) return `${days}D`;
+};
+
+export const handlePostLike = async (
+  postAction,
+  postId,
+  dispatch,
+  token,
+  postBy
+) => {
+  try {
+    if (postAction === "dec") {
+      await dislikePost(postId, token);
+      dispatch(updatePostLike({ id: postId, count: -1 }));
+      dispatch(updateLikedPostsForDislike(postId));
+    } else {
+      await likePost(postId, token, postBy.id);
+      dispatch(updatePostLike({ id: postId, count: 1 }));
+      dispatch(updateLikedPosts(postId));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const handleBookmark = async (postAction, postId, token, dispatch) => {
+  try {
+    if (postAction === "remove") {
+      await removePostFromBookmark(postId, token);
+      dispatch(removeFromBookmark(postId, token));
+      toast.success("Post removed from bookmark");
+    } else {
+      await bookmarkPost(postId, token);
+      dispatch(addToBookmark(postId, token));
+      toast.success("Post bookmarked");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
